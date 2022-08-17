fe3f549e4779305660056f609b6f56911d6b1bf2
a <48882585+DMClVG@users.noreply.github.com>
2022-08-17 17:20:00 +0200

    Remove debug print

--- client/lib/g3d/init.lua
+++ client/lib/g3d/init.lua
@@ -45,8 +45,6 @@ g3d = {
     shaderpath = ... .. "/g3d.vert",
 }
 
-print(g3d.shaderpath)
-
 -- the shader is what does the heavy lifting, displaying 3D meshes on your 2D monitor
 g3d.shader = love.graphics.newShader(g3d.shaderpath)
 g3d.newModel = require(g3d.path .. "/model")
